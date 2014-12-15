8f3e06373b18afed35e8b2d63dd6d760c399ef38
Dietmar Watzinger <dwatzinger@student.tugraz.at>
2014-12-16 00:33:05 +0100

    implement database

--- read_wiki_history.py
+++ read_wiki_history.py
@@ -36,9 +36,7 @@ def crawl_page(website, conn):
 				infofile.retrieve(websitename + link, "temp/page.sql.gz")
 				lookup = readPageDetails("temp/page.sql.gz")
 				os.remove("temp/page.sql.gz")
-				print len(lookup)
 	for link in linkset:
-		print len(lookup)
 		i = 1
 		zipfile = urllib.URLopener()
 		print "download meta-history file " + str(i) + "..."
@@ -49,11 +47,7 @@ def crawl_page(website, conn):
 		i = i + 1
 
 def read_file(filename, conn, lookup):
-	'''
-	txtfile = open(filename, 'r')
-	pagefile = open("page.txt", 'w')
-	'''
-
+	
 	handler = bz2.BZ2File(filename,'r')
 
 	title = None
@@ -69,28 +63,33 @@ def read_file(filename, conn, lookup):
 	y = 0
 	i = 0
 
-	#for line in txtfile:
 	for line in handler:
 		line = line.lstrip()
 		i = i + 1
-		if i%470000 == 0:
-			print '\r' + 'read History! Process: ' + str(float(i)/470000) + '%',
+		if i%100000 == 0:
+			print '\r' + 'read History! Process: ' + str(float(i)/1000000) + 'm rows',
 			sys.stdout.flush()
 		if line.startswith("<title>") == 1:
 			title = line[7:-9]
 			page = 1
 			previous_links = None
-			#print "Title: %s" %(title)
 		if line.startswith("<ns>") == 1 and page == 1:
 			namespace = line[4:-6]
 		if line.startswith("<id>") == 1 and page == 1:
 			page_id = line[4:-6]
 			page = 2
 		if line.startswith("<timestamp>"):
-			timest = datetime.datetime.strptime(line[11:-13], "%Y-%m-%dT%H:%M:%SZ")
+			timestamp = datetime.datetime.strptime(line[11:-13], "%Y-%m-%dT%H:%M:%SZ")
 			if page == 2:
 				page = 0
-				#pagefile.write(title + "\t" + page_id + "\t" + timestamp + "\n")
+				try:
+					title = title.lower()
+					title.replace(" ", "_")
+					lookup[title]
+					ins = table_pages.insert().values(title=title, page_id=page_id, timestamp=timestamp)
+					result = conn.execute(ins)
+				except:
+					pass
 		if line.startswith("<text"):
 			text = line
 			old_links = set()
@@ -108,28 +107,29 @@ def read_file(filename, conn, lookup):
 					link = link.replace(" ", "_")
 					try:
 						link_id = lookup[link]
+						ins = table_links.insert().values(page_id=int(page_id), link_id=link_id, timestamp=timestamp, add_remove=1)
+						result = conn.execute(ins)
 						x = x + 1
 					except:
 						y = y + 1
-						#print "not found"
-						#print link
-						#print title
-					'''
-					ins = table_links.insert().values(page_id=int(page_id), link=link, timestamp=timest, add_remove=1)
-					result = conn.execute(ins)
 				for link in old_links:
-					print "%s \t %s \t %s \t %d" %(page_id, link, timestamp, 0)
-					ins = table_links.insert().values(page_id=int(page_id), link=link, timestamp=timestamp, add_remove=0)
-					result = conn.execute(ins)'''
+					if ":" in link:
+						continue
+					link = link.lower()
+					link = link.replace(" ", "_")
+					try:
+						link_id = lookup[link]
+						ins = table_links.insert().values(page_id=int(page_id), link_id=link_id, timestamp=timestamp, add_remove=0)
+						result = conn.execute(ins)
+					except:
+						pass
 				previous_links = links
-			#print "%s \t %s" %(page_id, text)
-	print '\r' + 'read History! Process: Finish!'
+
+	print '\r' + 'read History! Process: Finish!     '
 	print"found"
 	print x
 	print "not found"
 	print y
-	#txtfile.close()
-	#pagefile.close()
 	handler.close()
 
 
@@ -152,38 +152,54 @@ def readPageDetails(filename):
 					name = name.replace(" ", "_")
 					id_list.append(insert[0])
 					name_list.append(name)
-					print name
 					i = i + 1
-					if i%112000 == 0:
-						print '\r' + 'read link list! Process: ' + str(float(i)/112000) + '%',
+					if i%100000 == 0:
+						print '\r' + 'read link list! Process: ' + str(float(i)/1000000) + 'm rows',
 						sys.stdout.flush()
-	print '\r' + 'read link list! Finish!'
+	print '\r' + 'read link list! Finish!     '
 	lookup = dict(zip(name_list, id_list))
-	#txtfile.close()
 	f.close()
 	return lookup
 
 
 print "start"
+
+website = "https://dumps.wikimedia.org/pdcwiki/20141118/"
+user = "postgres"
+password = ""
+language = website.split('/')[3]
+print language
+
+
+#create new DB
+engine = create_engine('postgres://' + user + ':' + password + '@localhost/postgres')
+conn = engine.connect()
+conn.execute("commit")
+conn.execute("create database " + language)
+conn.close()
+
 #connect to DB
-engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('postgresql://' + user + ':' + password + '@localhost/' + language)
 conn = engine.connect()
 metadata = MetaData()
-#create table
+
+#create table link
 table_links = Table('links', metadata,
-		Column('id', Integer, primary_key=True),
 		Column('page_id', Integer),
-		Column('link', String),
+		Column('link_id', Integer),
 		Column('timestamp', DateTime),
 		Column('add_remove', Integer))
+
+#create table title
+table_pages = Table('pages', metadata,
+		Column('title', String),
+		Column('page_id', Integer, primary_key=True),
+		Column('timestamp', DateTime))
+
 metadata.create_all(engine)
 
+crawl_page(website, conn)
 
-crawl_page("https://dumps.wikimedia.org/pdcwiki/20141118/", conn)
-#lookup = readPageDetails("/windows/C/Users/Digikong/Desktop/Didikong/Uni/Master/Masterprojekt/enwiki/enwiki-20140903-page.sql")
-#titles = read_titles("/windows/C/Users/Digikong/Desktop/Didikong/Uni/Master/Masterprojekt/enwiki/enwiki-20140903-all-titles")
-#read_file("wiki_source/mn/mnwiki-short-history.xml")
-#read_file("/windows/C/Users/Digikong/Desktop/Didikong/Uni/Master/Masterprojekt/enwiki/enwiki-20140903-pages-meta-history3.xml-p000053453p000055000", conn, lookup)
 print "end"
 
 
