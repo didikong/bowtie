4b370a208c69689f1b78d6c78d5137e696ae3841
Dietmar Watzinger <dwatzinger@student.tugraz.at>
2014-12-15 17:52:48 +0100

    add download files

--- read_wiki_history.py
+++ read_wiki_history.py
@@ -1,12 +1,60 @@
 import re
+import os
 import time, datetime
+import sys
+import bz2
+import urllib
+import gzip
 
+from BeautifulSoup import BeautifulSoup
+from urllib2 import urlopen
 from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, DateTime
 
-def read_file(filename, conn, titles):
+
+def crawl_page(website, conn):
+	websitename = "https://dumps.wikimedia.org"
+	linkset = set()
+	urlfile = urlopen(website)
+	content = urlfile.read()
+	soup = BeautifulSoup(content)
+	lookup = None
+	for li in soup.findAll('li', {'class':'file'} ):
+		hrefs=li.findAll('a')
+		for href in hrefs:
+			link = str(href)[9:]
+			try:
+				s = link.index('"')
+				link = link[:s]
+			except:
+				continue
+			if "pages-meta-history" in link and link.endswith(".bz2"):
+				linkset.add(websitename + link)
+			if "page.sql" in link:
+				print "download page informations..."
+				infofile = urllib.URLopener()
+				infofile.retrieve(websitename + link, "temp/page.sql.gz")
+				lookup = readPageDetails("temp/page.sql.gz")
+				os.remove("temp/page.sql.gz")
+				print len(lookup)
+	for link in linkset:
+		print len(lookup)
+		i = 1
+		zipfile = urllib.URLopener()
+		print "download meta-history file " + str(i) + "..."
+		zipfile.retrieve(link, "temp/file" + str(i) + ".bz2")
+		print "extract file " + str(i) + "..."
+		read_file("temp/file" + str(i) + ".bz2", conn, lookup)
+		os.remove("temp/file" + str(i) + ".bz2")
+		i = i + 1
+
+def read_file(filename, conn, lookup):
+	'''
 	txtfile = open(filename, 'r')
 	pagefile = open("page.txt", 'w')
+	'''
+
+	handler = bz2.BZ2File(filename,'r')
 
 	title = None
 	namespace = None
@@ -20,11 +68,14 @@ def read_file(filename, conn, titles):
 	x = 0
 	y = 0
 	i = 0
-	for line in txtfile:
+
+	#for line in txtfile:
+	for line in handler:
 		line = line.lstrip()
 		i = i + 1
 		if i%470000 == 0:
-			print float(i)/470000
+			print '\r' + 'read History! Process: ' + str(float(i)/470000) + '%',
+			sys.stdout.flush()
 		if line.startswith("<title>") == 1:
 			title = line[7:-9]
 			page = 1
@@ -51,43 +102,66 @@ def read_file(filename, conn, titles):
 					new_links = links.difference(previous_links)
 					old_links = previous_links.difference(links)
 				for link in new_links:
-					if link.replace(" ", "_") in titles:
+					if ":" in link:
+						continue
+					link = link.lower()
+					link = link.replace(" ", "_")
+					try:
+						link_id = lookup[link]
 						x = x + 1
-						print "found"
-						print link
-					else:
+					except:
 						y = y + 1
-						print "not found"
-						print link
-					'''print "%s \t %s \t %s \t %d" %(page_id, link, timestamp, 1)
+						#print "not found"
+						#print link
+						#print title
+					'''
 					ins = table_links.insert().values(page_id=int(page_id), link=link, timestamp=timest, add_remove=1)
-					result = conn.execute(ins)'''
-				'''for link in old_links:
+					result = conn.execute(ins)
+				for link in old_links:
 					print "%s \t %s \t %s \t %d" %(page_id, link, timestamp, 0)
 					ins = table_links.insert().values(page_id=int(page_id), link=link, timestamp=timestamp, add_remove=0)
 					result = conn.execute(ins)'''
 				previous_links = links
 			#print "%s \t %s" %(page_id, text)
-		if i > 1000000:
-			break
-	print "found"
+	print '\r' + 'read History! Process: Finish!'
+	print"found"
 	print x
 	print "not found"
 	print y
-	txtfile.close()
-	pagefile.close()
+	#txtfile.close()
+	#pagefile.close()
+	handler.close()
 
 
-def read_titles(filename):
-	txtfile = open(filename, 'r')
-	titles = set()
+
+def readPageDetails(filename):
+	#txtfile = open(filename, 'r')
+	f = gzip.open(filename, 'r')
+	name_list = list()
+	id_list = list()
 	i = 0
-	for line in txtfile:
-		titles.add(line.replace("\n", ""))
-		i = i + 1
-		if i%337000 == 0:
-			print float(i)/337000
-	return titles
+	for line in f:
+		if line.startswith("INSERT") == 1:
+			line = line[27:]
+			data = line.split("),(")
+			for insert_line in data:
+				insert = insert_line.split(',')
+				if insert[1] == '0':
+					name = insert[2].replace("'", "")
+					name = name.lower()
+					name = name.replace(" ", "_")
+					id_list.append(insert[0])
+					name_list.append(name)
+					print name
+					i = i + 1
+					if i%112000 == 0:
+						print '\r' + 'read link list! Process: ' + str(float(i)/112000) + '%',
+						sys.stdout.flush()
+	print '\r' + 'read link list! Finish!'
+	lookup = dict(zip(name_list, id_list))
+	#txtfile.close()
+	f.close()
+	return lookup
 
 
 print "start"
@@ -104,9 +178,12 @@ table_links = Table('links', metadata,
 		Column('add_remove', Integer))
 metadata.create_all(engine)
 
-titles = read_titles("/windows/C/Users/Digikong/Desktop/Didikong/Uni/Master/Masterprojekt/enwiki/enwiki-20140903-all-titles")
+
+crawl_page("https://dumps.wikimedia.org/pdcwiki/20141118/", conn)
+#lookup = readPageDetails("/windows/C/Users/Digikong/Desktop/Didikong/Uni/Master/Masterprojekt/enwiki/enwiki-20140903-page.sql")
+#titles = read_titles("/windows/C/Users/Digikong/Desktop/Didikong/Uni/Master/Masterprojekt/enwiki/enwiki-20140903-all-titles")
 #read_file("wiki_source/mn/mnwiki-short-history.xml")
-read_file("/windows/C/Users/Digikong/Desktop/Didikong/Uni/Master/Masterprojekt/enwiki/enwiki-20140903-pages-meta-history3.xml-p000053453p000055000", conn, titles)
+#read_file("/windows/C/Users/Digikong/Desktop/Didikong/Uni/Master/Masterprojekt/enwiki/enwiki-20140903-pages-meta-history3.xml-p000053453p000055000", conn, lookup)
 print "end"
 
 
